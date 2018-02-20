b8be40ad536bf54540d86b9c84bd8001a671585b
Vinit Agarwal <123vinit.agarwal@gmail.com>
2018-02-20 21:52:04 +0530

    Added Comments Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    //Material Design Modules
     MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+
     FlexLayoutModule
   ],
   providers: [],
