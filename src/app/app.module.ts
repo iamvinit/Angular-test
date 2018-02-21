1befb19abcd95e75eecd44cfe6a12c5aab9b84df
Vinit Agarwal <123vinit.agarwal@gmail.com>
2018-02-21 20:33:18 +0530

    modified to SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,17 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 
 
 @NgModule({
@@ -25,7 +32,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +49,8 @@ import { FooterComponent } from './footer/footer.component';
     MatGridListModule,
     MatCardModule,
 
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
